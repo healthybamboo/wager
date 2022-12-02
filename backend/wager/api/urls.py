c24db3f073c8025bb4c9ba6e7c5e84d4f1c36d71
takeru <healthybamboo@icloud.com>
2022-12-02 23:57:10 +0900

    🚚  収支一覧に関するパスを変更

--- backend/wager/api/urls.py
+++ backend/wager/api/urls.py
@@ -13,13 +13,11 @@ urlpatterns = [
     path('user/update',user_view.Update.as_view()),
 
     # 収支一覧(POST,GET)
-    path('spending/',spending_view.SpendingList.as_view()),
+    path('spendinglist/',spending_view.SpendingList.as_view()),
     
     # 収支詳細(POST,GET,PUT,DELETE)
-    # path('spending/<int:pk>/',spending_view.SpendingDetail.as_view()),
-    
-    # 
-    # path('spending/<int:pk>/detail/',spending_view.SpendingDetailList.as_view()),
+    path('spending/<int:pk>/',spending_view.SpendingDetail.as_view()),
+     # path('spending/<int:pk>/detail/',spending_view.SpendingDetailList.as_view()),
     # path('spending/<int:pk>/detail/<int:pk2>/',spending_view.SpendingDetailDetail.as_view()),
     # path('spending/<int:pk>/detail/<int:pk2>/delete/',spending_view.SpendingDetailDetail.as_view()),
     
