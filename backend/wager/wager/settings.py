508ccad51a615f8bef6bc981e86c10e2705534f0
takeru <healthybamboo@icloud.com>
2022-12-05 18:24:50 +0900

    ✨ CORS対策を追加

--- backend/wager/wager/settings.py
+++ backend/wager/wager/settings.py
@@ -43,14 +43,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -76,6 +78,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wager.wsgi.application'
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:9000',
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
